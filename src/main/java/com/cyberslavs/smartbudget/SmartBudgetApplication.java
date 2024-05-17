9b1fb3910045bbfd91ab453b7275fb5998098ef0
public_hysteria <juuzodes@yandex.ru>
2024-05-17 13:27:27 +0300

    Initial commit

---
+++ src/main/java/com/cyberslavs/smartbudget/SmartBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.cyberslavs.smartbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBudgetApplication.class, args);
+	}
+
+}
