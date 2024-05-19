4cd1e69a9047117eae797f62db93f2cbd428f3f6
public_hysteria <juuzodes@yandex.ru>
2024-05-19 08:41:46 +0300

    cors

--- server/src/main/java/com/cyberslavs/smartbudget/SmartBudgetApplication.java
+++ server/src/main/java/com/cyberslavs/smartbudget/SmartBudgetApplication.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -31,6 +34,15 @@ public class SmartBudgetApplication {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SmartBudgetApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("*");
+			}
+		};
 	}
 
 }
